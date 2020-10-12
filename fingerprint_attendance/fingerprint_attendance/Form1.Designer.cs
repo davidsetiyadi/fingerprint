2b401af29d6422466b5d156cbab07667a7f55acc
David Setiyadi <davidsetiadi11@gmail.com>
2020-10-12 09:07:32 +0700

    Update set fingerprint

---
+++ fingerprint_attendance/fingerprint_attendance/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace fingerprint_attendance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtReaderSelected = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.btnReaderSelect = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIdentify = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtReaderSelected
+            // 
+            this.txtReaderSelected.AutoCompleteCustomSource.AddRange(new string[] {
+            "{F769E5CC-4D99-FF45-B09B-9771A0F6AC48}"});
+            this.txtReaderSelected.Font = new System.Drawing.Font("Tahoma", 8F);
+            this.txtReaderSelected.Location = new System.Drawing.Point(303, 333);
+            this.txtReaderSelected.Name = "txtReaderSelected";
+            this.txtReaderSelected.ReadOnly = true;
+            this.txtReaderSelected.Size = new System.Drawing.Size(233, 20);
+            this.txtReaderSelected.TabIndex = 29;
+            // 
+            // Label1
+            // 
+            this.Label1.Location = new System.Drawing.Point(300, 315);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(236, 15);
+            this.Label1.TabIndex = 30;
+            this.Label1.Text = "Selected Reader:";
+            // 
+            // btnReaderSelect
+            // 
+            this.btnReaderSelect.Location = new System.Drawing.Point(307, 284);
+            this.btnReaderSelect.Name = "btnReaderSelect";
+            this.btnReaderSelect.Size = new System.Drawing.Size(115, 23);
+            this.btnReaderSelect.TabIndex = 28;
+            this.btnReaderSelect.Text = "Reader Selection";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 284);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CloseButton.Location = new System.Drawing.Point(439, 284);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 26;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(307, 40);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(217, 220);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 25;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(300, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(236, 15);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Please Place Finger  On Fingerprint";
+            // 
+            // txtIdentify
+            // 
+            this.txtIdentify.Location = new System.Drawing.Point(13, 16);
+            this.txtIdentify.Multiline = true;
+            this.txtIdentify.Name = "txtIdentify";
+            this.txtIdentify.Size = new System.Drawing.Size(269, 244);
+            this.txtIdentify.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 369);
+            this.Controls.Add(this.txtReaderSelected);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.btnReaderSelect);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIdentify);
+            this.Name = "Form1";
+            this.Text = "Check In";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.TextBox txtReaderSelected;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button btnReaderSelect;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox txtIdentify;
+    }
+}
+
